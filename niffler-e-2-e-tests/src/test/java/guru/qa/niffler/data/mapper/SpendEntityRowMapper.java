21522d8ec235caecc4bb717ca22a7bd5a8e6955e
Сычев Михаил Валерьевич <msychev@mts.ru>
2025-07-14 11:00:14 +0300

    remove comment

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/mapper/SpendEntityRowMapper.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/mapper/SpendEntityRowMapper.java
@@ -21,7 +21,6 @@ public class SpendEntityRowMapper implements RowMapper<SpendEntity> {
 		se.setId(rs.getObject("id", UUID.class));
 		se.setUsername(rs.getString("username"));
 		se.setSpendDate(rs.getObject("spend_date", Date.class));
-//		se.setCurrency(rs.getObject("currency", CurrencyValues.class));
 		se.setCurrency(CurrencyValues.valueOf(rs.getString("currency")));
 		se.setAmount(rs.getDouble("amount"));
 		se.setDescription(rs.getString("description"));
